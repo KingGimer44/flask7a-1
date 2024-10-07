117dae2e76f624e54e780415fe63ca473f409d84
KingGimer44 <150565033+KingGimer44@users.noreply.github.com>
2024-10-07 18:50:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,160 +1,210 @@
-from flask import Flask
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-from flask import render_template
-from flask import request
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-import pusher
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-import mysql.connector
-import datetime
-import pytz
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 
-con = mysql.connector.connect(
-  host="185.232.14.52",
-  database="u760464709_tst_sep",
-  user="u760464709_tst_sep_usr",
-  password="dJ0CIAFF="
-)
+    <title>App</title>
+</head>
+<body>
+    <div class="container">
+      <form id="frmTemperaturaHumedad" method="post">
+          <input type="hidden" id="id" name="id">
 
-app = Flask(__name__)
+          <div class="mb-1">
+              <label for="temperatura">Temperatura</label>
+              <input type="number" id="temperatura" name="temperatura" class="form-control">
+          </div>
+          <div class="mb-1">
+              <label for="humedad">Humedad</label>
+              <input type="number" id="humedad" name="humedad" class="form-control">
+          </div>
+          <div class="mb-1">
+              <button id="guardar" name="guardar" class="btn btn-dark">Guardar</button>
+              <button type="reset" id="cancelar" name="cancelar" class="btn btn-link">Cancelar</button>
+          </div>
+      </form>
+      <table class="table table-sm">
+        <thead>
+          <tr>
+            <th>Temperatura</th>
+            <th>Humedad</th>
+            <th>Fecha</th>
+            <th>Hora</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody id="tbodyTemperaturaHumedad"></tbody>
+      </table>
+    </div>
 
-@app.route("/")
-def index():
-    con.close()
-    return render_template("app.html")
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-@app.route("/alumnos")
-def alumnos():
-    con.close()
-    return render_template("alumnos.html")
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <div class="app-float-button bg-body" style="z-index: 3; position: fixed; bottom: 5px; left: 5px; cursor: pointer;">
+        <ul class="list-group list-group-horizontal">
+            <li class="list-group-item" data-bs-theme-value="light">
+                <i class="bi bi-sun-fill"></i>
+            </li>
+            <li class="list-group-item" data-bs-theme-value="dark">
+                <i class="bi bi-moon-stars-fill"></i>
+            </li>
+            <li class="list-group-item" data-bs-theme-value="auto">
+                <i class="bi bi-circle-half"></i>
+            </li>
+        </ul>
+    </div>
+    <script>
+        /*!
+        * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+        * Copyright 2011-2022 The Bootstrap Authors
+        * Licensed under the Creative Commons Attribution 3.0 Unported License.
+        */
 
-@app.route("/alumnos/guardar", methods=["POST"])
-def alumnosGuardar():
-    con.close()
-    matricula      = request.form["txtMatriculaFA"]
-    nombreapellido = request.form["txtNombreApellidoFA"]
-    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
-def notificarActualizacionReserva():
-    pusher_client = pusher.Pusher(
-        app_id='1767967',
-        key='34091ea15b1a362fb38d',
-        secret='9a986831a832e499c9e4',
-        cluster='us2',
-        ssl=True
-    )
+        /** Reescrito */
 
-    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", {})
+        var bootstrapTheme = localStorage.getItem("theme")
 
-@app.route("/buscar")
-def buscar():
-    if not con.is_connected():
-        con.reconnect()
+        function getPreferredTheme() {
+            if (bootstrapTheme) {
+                return bootstrapTheme
+            }
 
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM sensor_log")
-    con.close()
-    
-    registros = cursor.fetchall()
+            return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+        }
 
-    return registros
+        function setTheme(theme) {
+            if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                document.documentElement.setAttribute("data-bs-theme", "dark")
+            }
+            else {
+                document.documentElement.setAttribute("data-bs-theme", ((theme == "auto") ? "light" : theme))
+            }
+        }
 
-# @app.route("/evento", methods=["GET"])
-# def evento():
-#     if not con.is_connected():
-#         con.reconnect()
+        function showActiveTheme(theme) {
+            $("[data-bs-theme-value]").removeClass("bg-primary text-white active")
+            $(`[data-bs-theme-value="${theme}"]`).addClass("bg-primary text-white active")
+        }
 
-#     cursor = con.cursor()
+        $(document).on("click", '[data-bs-theme-value]', function (event) {
+            const theme = this.getAttribute("data-bs-theme-value")
+            localStorage.setItem("theme", theme)
+            setTheme(theme)
+            showActiveTheme(theme)
+        })
 
-#     args = request.args
-  
-#     sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
-#     val = (args["temperatura"], args["humedad"], datetime.datetime.now())
-#     cursor.execute(sql, val)
-    
-#     con.commit()
-#     con.close()
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function (event) {
+            if (bootstrapTheme !== "light"
+            || bootstrapTheme !== "dark") {
+                setTheme(getPreferredTheme())
+            }
+        })
 
-#     pusher_client = pusher.Pusher(
-#         app_id="1714541",
-#         key="cda1cc599395d699a2af",
-#         secret="9e9c00fc36600060d9e2",
-#         cluster="us2",
-#         ssl=True
-#     )
-    
-#     pusher_client.trigger("conexion", "evento", request.args)
+        document.addEventListener("DOMContentLoaded", function (event) {
+            setTheme(bootstrapTheme)
+            showActiveTheme(getPreferredTheme())
+        })
+    </script>
 
-@app.route("/guardar", methods=["POST"])
-def guardar():
-    if not con.is_connected():
-        con.reconnect()
+    <script>
+        window.addEventListener("load", function (event) {
+            function buscar() {
+                $.get("/buscar", function (respuesta) {
+                    $("#tbodyTemperaturaHumedad").html("")
 
-    id          = request.form["id"]
-    temperatura = request.form["temperatura"]
-    humedad     = request.form["humedad"]
-    fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
-    
-    cursor = con.cursor()
+                    var fechaAnterior = ""
 
-    if id:
-        sql = """
-        UPDATE tst0_resevas SET
-        Nombre_Apellido = %s,
-        Telefono     = %s
-        WHERE Id_Reserva = %s
-        """
-        val = (nombre_apellido, telefono, id)
-    else:
-        sql = """INSERT INTO tst0_resevas (Nombre_Apellido, Telefono, Fecha_Hora)
-                                 VALUES (%s,          %s,      %s)"""
-        val =                           (nombre_apellido, telefono, fechahora)
-    
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
+                    for (var x in respuesta) {
+                        var temperaturaHumedad = respuesta[x]
 
-    notificarActualizacionReserva()
+                        var fecha = ""
+                        var fechaTemp = temperaturaHumedad["Fecha"]
 
-    return make_response(jsonify({}))
-  
-@app.route("/editar", methods=["GET"])
-def editar():
-    if not con.is_connected():
-        con.reconnect()
+                        if (fechaAnterior != fechaTemp) {
+                            fecha         = fechaTemp
+                            fechaAnterior = fechaTemp
+                        }
 
-    id = request.args["id"]
+                        $("#tbodyTemperaturaHumedad").append(`<tr>
+                            <td>${temperaturaHumedad["Temperatura"]}</td>
+                            <td>${temperaturaHumedad["Humedad"]}</td>
+                            <td>${fecha}</td>
+                            <td>${temperaturaHumedad["Hora"]}</td>
+                            <td>
+                                <button class="btn btn-primary btn-editar" data-id="${temperaturaHumedad["Id_Log"]}">Editar</button>
+                            </td>
+                        </tr>`)
+                    }
+                })
+            }
 
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT Id_Reserva, Nombre_Apellido, Telefono FROM tst0_resevas
-    WHERE Id_Reserva = %s
-    """
-    val    = (id,)
+            buscar()
 
-    cursor.execute(sql, val)
-    registros = cursor.fetchall()
-    con.close()
+            $(document).on("click", ".btn-editar", function (event) {
+                var id = $(this).attr("data-id")
 
-    return make_response(jsonify(registros))
+                $.get("/editar", {id: id}, function (respuesta) {
+                    console.log(respuesta)
+                    var temperaturaHumedad = respuesta[0]
 
-@app.route("/eliminar", methods=["POST"])
-def eliminar():
-    if not con.is_connected():
-        con.reconnect()
+                    $("#id")
+                    .val(temperaturaHumedad["Id_Log"])
+                    .trigger("focus")
 
-    id = request.form["id"]
+                    $("#temperatura").val(temperaturaHumedad["Temperatura"])
+                    $("#humedad").val(temperaturaHumedad["Humedad"])
+                })
+            })
+            
+            $("#frmTemperaturaHumedad")
+            .submit(function (event)  {
+                event.preventDefault()
 
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    DELETE FROM tst0_resevas
-    WHERE Id_Reserva = %s
-    """
-    val    = (id,)
+                $.post("/guardar", $(this).serialize(), function (respuesta) {
+                    $("#frmTemperaturaHumedad").get(0).reset()
+                })
+            })
+            .on("reset", function (event) {
+                $("#id").val("")
+            })
 
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
+            Pusher.logToConsole = true
+            
+            var pusher = new Pusher("cda1cc599395d699a2af", {
+                cluster: "us2"
+            })
 
-    notificarActualizacionTemperaturaHumedad()
+            // Canal ---> Donde se concentran los hilos con los clientes conectados
+            var channel = pusher.subscribe("canalRegistrosTemperaturaHumedad");
 
-    return make_response(jsonify({}))
+            // Evento --> El código que se accionará con la información enviada o el uso del canal
+            channel.bind("registroTemperaturaHumedad", function (temperaturaHumedad) {
+                // 1. Notificar el evento
+                // alert("Ocurrió el evento")
+
+                // 2. Recibir información enviada en el evento
+                // console.log(temperaturaHumedad)
+
+                // 3. Trabajar información enviada por el evento
+                /**
+                $("#tbodyTemperaturaHumedad").prepend(`<tr>
+                    <td>${temperaturaHumedad.temperatura}</td>
+                    <td>${temperaturaHumedad.humedad}</td>
+                    <td></td>
+                </tr>`)
+                */
+
+                // 4. Mandar a llamar otro código luego del evento
+                buscar()
+            })
+        })
+    </script>    
+</body>
+</html>
